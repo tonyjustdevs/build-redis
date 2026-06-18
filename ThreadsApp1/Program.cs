34fad480c74a39eb3b241b33aa85d0fad5b11504
tonyjustdevs <tonyjustdevs@gmail.com>
2026-06-18 21:01:30 +0700

    add raw thread with dg -closes #64

---
+++ ThreadsApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace ThreadsApp1;
+
+//delegate void ShoutSoDelegate(string input_str);
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TaskIssues64();
+
+    }
+    public static void TaskIssues64()
+    {
+
+        Console.WriteLine($"[Main] Started TaskIssues64() [{Thread.CurrentThread.ManagedThreadId}]");
+        Thread thread = new Thread(TPSH);
+        thread.Start();
+        thread.Join();
+        Console.WriteLine($"[Main] Completed TaskIssues64() [{Thread.CurrentThread.ManagedThreadId}]");
+    }
+
+    public static void TPSH()
+    {
+        Console.WriteLine($"[TPSH] sup 69! [{Thread.CurrentThread.ManagedThreadId}]");
+    }
+
+}
